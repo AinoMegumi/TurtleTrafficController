5dd3c9e6c95619cd2ba75ca055f3366cd6c98ec1
AinoMegumi <aimegu2014@gmail.com>
2022-04-22 00:45:07 +0900

    GCCでビルドできるようにコード修正

--- GetIterator.hpp
+++ GetIterator.hpp
@@ -4,13 +4,13 @@
 #include <unordered_map>
 
 template<typename Key, typename Value>
-static std::unordered_map<Key, Value>::iterator GetIterator(std::unordered_map<Key, Value>& m, const Key& KeyData) {
+static typename std::unordered_map<Key, Value>::iterator GetIterator(std::unordered_map<Key, Value>& m, const Key& KeyData) {
 	typename std::unordered_map<Key, Value>::iterator it = m.find(KeyData);
 	if (it == m.end()) throw HttpException(404);
 	return it;
 }
 template<typename Key, typename Value>
-static std::unordered_map<Key, Value>::const_iterator GetIterator(const std::unordered_map<Key, Value>& m, const Key& KeyData) {
+static typename std::unordered_map<Key, Value>::const_iterator GetIterator(const std::unordered_map<Key, Value>& m, const Key& KeyData) {
 	typename std::unordered_map<Key, Value>::const_iterator it = m.find(KeyData);
 	if (it == m.end()) throw HttpException(404);
 	return it;
